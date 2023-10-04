87cbb4d221e9e6ce0bf393ec370d70d25d0f1d96
Schaene Trible <schaene@gmx.com>
2023-10-04 00:34:33 -0500

    Added library installers
    Run the script to install the needed libs

---
+++ src/libs/Install-Libs-Windows.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Downloading Raylib-J.jar...
+curl -L -o Raylib-J.jar https://github.com/CreedVI/Raylib-J/releases/download/v0.5/Raylib-J.jar
+
+echo Downloading Jamepad.jar...
+curl -L -o Jamepad.jar https://github.com/williamahartman/Jamepad/releases/download/1.3.2/Jamepad.jar
+
+echo Files downloaded successfully.
